97dd40a3df5a609622f8306e99acc5da821b28ce
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-04-05 11:10:42 +0100

    just commit - Finance-management-app

--- server.js
+++ server.js
@@ -1,37 +1,37 @@
-// // server.js
-// const express = require('express');
-// const mongoose = require('mongoose');
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const userRoutes = require('./routes/user');
-// require('dotenv').config(); 
-// const userExpense = require('./routes/expense');
-// const userGoal = require('./routes/budegetAndGoals');
-// const userIncome = require('./routes/income');
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const userRoutes = require('./routes/user');
+require('dotenv').config(); 
+const userExpense = require('./routes/expense');
+const userGoal = require('./routes/budegetAndGoals');
+const userIncome = require('./routes/income');
 
-// const app = express();
-// const port = process.env.PORT || 5000;
-
-
-// // Middleware
-// app.use(bodyParser.json());
-// app.use(cors());
-
-// // Routes
-// app.use('/api/users', userRoutes);
-// app.use('/api/expense', userExpense);
-// app.use('/api/income', userIncome);
-// app.use('/api/budgetAndGoals', userGoal);
+const app = express();
+const port = process.env.PORT || 5000;
 
 
+// Middleware
+app.use(bodyParser.json());
+app.use(cors());
 
-// // Database connection
-// // mongoose.connect('mongodb://localhost:27017/auth-db', { useNewUrlParser: true, useUnifiedTopology: true })
-// mongoose.connect(process.env.MONGODB_URI)
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.log(err));
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/expense', userExpense);
+app.use('/api/income', userIncome);
+app.use('/api/budgetAndGoals', userGoal);
 
-// app.listen(port, () => {
-//   console.log(`Server running on port ${port}`);
-// });
+
+
+// Database connection
+// mongoose.connect('mongodb://localhost:27017/auth-db', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
