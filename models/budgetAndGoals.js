fd713cf0ccf0b09c76197a31c17e3d56029edf57
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-04-11 11:15:04 +0100

    just commit - Finance-management-app

--- models/budgetAndGoals.js
+++ models/budgetAndGoals.js
@@ -1,30 +1,30 @@
-// // models/budgetAndGoal.js
+// models/budgetAndGoal.js
 
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// const budgetSchema = new Schema({
-//   category: {
-//     type: String,
-//     required: true,
-//   },
-//   date: {
-//     type: String,
-//     required: true,
-//   },
-//   amountb: {
-//     type: Number,
-//     required: true,
-//   },
-//   amounts: {
-//     type: Number,
-//     required: true,
-//   },
-//   userId: {
-//     type:String,
-//     ref: 'User',
-//     required: true,
-//   },
-// });
+const budgetSchema = new Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  amountb: {
+    type: Number,
+    required: true,
+  },
+  amounts: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type:String,
+    ref: 'User',
+    required: true,
+  },
+});
 
-// module.exports = mongoose.model('budget', budgetSchema);
+module.exports = mongoose.model('budget', budgetSchema);
