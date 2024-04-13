2911a3dca20a234596596d134d5f62e64ef04236
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-04-13 11:18:18 +0100

    just commit - Finance-management-app

--- models/Income.js
+++ models/Income.js
@@ -1,26 +1,26 @@
-// // models/Expense.js
+// models/Expense.js
 
-// const mongoose = require('mongoose');
-// const Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-// const incomeSchema = new Schema({
-//   category: {
-//     type: String,
-//     required: true,
-//   },
-//   date: {
-//     type: String,
-//     required: true,
-//   },
-//   amount: {
-//     type: Number,
-//     required: true,
-//   },
-//   userId: {
-//     type:String,
-//     ref: 'User',
-//     required: true,
-//   },
-// });
+const incomeSchema = new Schema({
+  category: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+  amount: {
+    type: Number,
+    required: true,
+  },
+  userId: {
+    type:String,
+    ref: 'User',
+    required: true,
+  },
+});
 
-// module.exports = mongoose.model('Income', incomeSchema);
+module.exports = mongoose.model('Income', incomeSchema);
