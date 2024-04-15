e2344bc7aada42d5ccaac93a336e6509f4a3a3e6
Jeremiah Adedeji <connectwithdevjerry@gmail.com>
2024-04-15 11:19:48 +0100

    just commit - Finance-management-app

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,28 @@
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
-// const auth = (req, res, next) => {
-//   // Get the token from the header
-//   const authHeader = req.header('Authorization');
+const auth = (req, res, next) => {
+  // Get the token from the header
+  const authHeader = req.header('Authorization');
 
-//   // Check if the token is not present
-//   if (!authHeader) {
-//     return res.status(401).json({ message: 'No token, authorization denied' });
-//   }
+  // Check if the token is not present
+  if (!authHeader) {
+    return res.status(401).json({ message: 'No token, authorization denied' });
+  }
 
-//   // Extract the token and handle any potential errors
-//   const token = authHeader.startsWith('Bearer ') ? authHeader.replace('Bearer ', '') : null;
-//   if (!token) {
-//     return res.status(401).json({ message: 'Invalid token format' });
-//   }
+  // Extract the token and handle any potential errors
+  const token = authHeader.startsWith('Bearer ') ? authHeader.replace('Bearer ', '') : null;
+  if (!token) {
+    return res.status(401).json({ message: 'Invalid token format' });
+  }
 
-//   try {
-//     // Verify the token
-//     const decoded = jwt.verify(token, 'your_jwt_secret');
-//     req.user = decoded;
-//     next();
-//   } catch (e) {
-//     res.status(400).json({ message: 'Token is not valid' });
-//   }
-// };
+  try {
+    // Verify the token
+    const decoded = jwt.verify(token, 'your_jwt_secret');
+    req.user = decoded;
+    next();
+  } catch (e) {
+    res.status(400).json({ message: 'Token is not valid' });
+  }
+};
 
-// module.exports = auth;
+module.exports = auth;
